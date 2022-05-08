9e5dca03d77cf1db6229fce2270f424504f6485f
Ali Harati <violydesigns@gmail.com>
2022-05-08 11:46:34 +0430

    Set up axios and fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/Form";
 import Persons from "./components/Persons";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searched, setSearched] = useState(persons);
   const [filter, setFilter] = useState("");
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((resonse) => {
+      setPersons(resonse.data);
+      setSearched(resonse.data);
+    });
+  };
+  useEffect(hook, []);
+
   const addNewNames = (event) => {
     event.preventDefault();
     if (persons.filter((person) => person === newName).length !== 0) {
